fd5606e336c42451c902ee6b1323b60e0f7dfd57
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-14 00:49:34 +0200

    "hotfix"

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,8 +2,8 @@
 """ Module for square class"""
 
 
-class square():
-    '''Square class'''
+class Square():
+    """ Square class """
     width = 0
     height = 0
 
@@ -14,20 +14,20 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return (self.width * self.height)
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ Perimeter of my square """
-        return ((self.width * 2) + (self.height * 2))
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Perimeter of my square """
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Creating a square object """
-    s = square(width=12, height=9)
+    """ Create a square object """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
