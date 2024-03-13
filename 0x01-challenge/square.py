96aa76c2a2e29e7759f69c83b8eb1b549510ecd4
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-14 00:48:41 +0200

    "hotfix"

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,6 +8,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,9 +17,11 @@ class square():
         return (self.width * self.height)
 
     def PermiterOfMySquare(self):
+        """ Perimeter of my square """
         return ((self.width * 2) + (self.height * 2))
 
     def __str__(self):
+        """ Perimeter of my square """
         return "{}/{}".format(self.width, self.height)
 
 
